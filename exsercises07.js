96d69366161dc7852539b373f4183a96f8b15124
alifbaguz03 <alifbaguz03@gmail.com>
2018-10-07 21:14:52 +0700

    tugas saya

---
+++ exsercises07.js
@@ -0,0 +1,29 @@
+
+//exsercise7
+/*
+function hitungJumlahKata(kalimat) {
+  return kalimat.split(' ').length;
+}
+console.log(hitungJumlahKata('I have a dream')); // 4
+console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+console.log(hitungJumlahKata('A song to sing')); // 4
+console.log(hitungJumlahKata('I')); // 1
+console.log(hitungJumlahKata('I believe I can code')); // 5
+*/
+/*
+function angkaPalindrome(num) {
+  while (true) {
+    num++;
+    var str = String(num);
+    var strReverse = str.split('').reverse().join('');
+    if (str === strReverse) {
+      return Number(str);
+    }
+  }
+}
+console.log(angkaPalindrome(8)); // 9
+console.log(angkaPalindrome(10)); // 11
+console.log(angkaPalindrome(117)); // 121
+console.log(angkaPalindrome(175)); // 181
+console.log(angkaPalindrome(1000)); // 1001
+*/
